dc8423c7d63a06317d915d144053996e8a6970f6
theowwx <ana.andrade.goncalves@escola.pr.gov.br>
2024-09-23 10:08:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+ background(135, 206, 235);
+}
+
+function draw() {
+  fill (0, 105, 148)
+  rect(0, 200, 400, 300)
+  fill(255, 223, 0)
+  circle (100, 148, 100)
+  fill ( 135, 206, 235)
+  noStroke(100, 100)
+}
